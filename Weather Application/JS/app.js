733bab286c09d9d4f2b031007b8e6c706ba01dd0
Vipul Katwal <vipulkatwal17@gmail.com>
2024-02-17 22:09:18 +0530

    first commit

---
+++ Weather Application/JS/app.js
@@ -0,0 +1,45 @@
+// adding API key and URL from the website
+const apiKey = "bc56830c1b30e51b66d02ae1d7075b8f";
+const apiUrl =
+	"https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+	const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+	if (response.status == 404) {
+		document.querySelector(".error").style.display = "block";
+		document.querySelector(".weather").style.display = "none";
+	} else {
+		var data = await response.json();
+
+		document.querySelector(".city").innerHTML = data.name;
+		document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+		document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+		document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+		if (data.weather[0].main == "Clouds") {
+			weatherIcon.src = "/Weather Application/Images/clouds.png";
+		} else if (data.weather[0].main == "Clear") {
+			weatherIcon.src = "/Weather Application/Images/clear.png";
+		} else if (data.weather[0].main == "Rain") {
+			weatherIcon.src = "/Weather Application/Images/rain.png";
+		} else if (data.weather[0].main == "Drizzle") {
+			weatherIcon.src = "/Weather Application/Images/drizzle.png";
+		} else if (data.weather[0].main == "Mist") {
+			weatherIcon.src = "/Weather Application/Images/mist.png";
+		}
+
+		document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+	}
+}
+searchBtn.addEventListener("click", () => {
+	checkWeather(searchBox.value);
+});
+
+checkWeather();
